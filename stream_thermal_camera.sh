787d9211368f5a858010e26c7b85fce5596f0786
dhanushka <liyanagedc@gmail.com>
2024-02-06 10:59:34 +0200

    rtsp url

--- stream_thermal_camera.sh
+++ stream_thermal_camera.sh
@@ -6,5 +6,5 @@ source /opt/ros/noetic/setup.bash
 
 source devel/setup.bash
 
-rosrun thermal_camera_streamer thermal_cam.py
+rosrun thermal_camera_streamer thermal_cam.py -rtsp://admin:Pineapple777@192.168.1.196:8554/ir.0
 
