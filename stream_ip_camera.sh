f250d2165bc6481ad914544b20b55308e2d38f05
dhanushka <liyanagedc@gmail.com>
2024-02-07 19:00:37 +0200

    ip camera shell script updated

--- stream_ip_camera.sh
+++ stream_ip_camera.sh
@@ -6,5 +6,10 @@ source /opt/ros/noetic/setup.bash
 
 source devel/setup.bash
 
-rosrun video_streamer vision.py
+rosrun video_streamer vision.py cam1 cam1_image rtsp://admin:Admin123@192.168.1.209:554/path
+
+sleep 5s
+
+# Run the second camera
+gnome-terminal -- rosrun video_streamer vision.py cam2 cam2_image rtsp://admin:Admin123@192.168.1.210:554/path
 
