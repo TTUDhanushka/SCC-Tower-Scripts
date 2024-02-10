b5d48ba1c36e334626c65f329477006b0fb8d1b5
dhanushka <liyanagedc@gmail.com>
2024-02-10 18:15:24 +0200

    Updated ros logging script to include all the starting scripts

--- ros_record.sh
+++ ros_record.sh
@@ -19,4 +19,6 @@ echo "Starting rosbag recording for all the published topics."
 
 echo "Use CTRL + C to stop recording."
 
-rosbag record -a
+# rosbag record -a
+rosbag record /sbg/imu_data /sbg/ekf_quat --split --duration=1h  /chatter
+
