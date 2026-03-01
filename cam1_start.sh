0c3c548b2f5c654231553a34d841862a44367fa5
dhanushka <liyanagedc@gmail.com>
2026-03-01 22:32:08 +0200

    Updated camera scripts

--- cam1_start.sh
+++ cam1_start.sh
@@ -6,4 +6,6 @@ source /opt/ros/noetic/setup.bash
 
 source devel/setup.bash
 
-rosrun video_streamer vision.py cam1 cam1_image rtsp://admin:Admin123@192.168.1.209:554/path
+# rosrun video_streamer vision.py cam1 cam1_image rtsp://admin:Admin123@192.168.1.209:554/path
+
+rosrun camera_publisher camera_publisher_node _url:="rtsp://admin:Admin123@192.168.1.211:554/path" _previewImg:="/cam1_image_preview" _rawImg:="/cam1_image" __name:=camera1
--- cam2_start.sh
+++ cam2_start.sh
@@ -6,4 +6,6 @@ source /opt/ros/noetic/setup.bash
 
 source devel/setup.bash
 
-rosrun video_streamer vision.py cam2 cam2_image rtsp://admin:Admin123@192.168.1.210:554/path
+# rosrun video_streamer vision.py cam2 cam2_image rtsp://admin:Admin123@192.168.1.210:554/path
+
+rosrun camera_publisher camera_publisher_node _url:="rtsp://admin:Admin123@192.168.1.209:554/path" _previewImg:="/cam2_image_preview" _rawImg:="/cam2_image" __name:=camera2
